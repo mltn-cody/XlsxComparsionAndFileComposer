1fbc73bdcc61d83fb57ab9e52d1522a020f6de69
Milton Cody <cody.mltn.dev@outlook.com>
2017-06-24 22:35:20 -0500

    fix data reader

--- XlsxComparsionAndFileComposer/Reader/XlsxReader.cs
+++ XlsxComparsionAndFileComposer/Reader/XlsxReader.cs
@@ -38,29 +38,27 @@ namespace XlsxComparsionAndFileComposer
             using (OleDbConnection objConn = new OleDbConnection(@" Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + fileName + "; Extended Properties = 'Excel 8.0;HDR=YES'"))
             {
                 objConn.Open();
-                OleDbCommand cmd = new OleDbCommand();
-                OleDbDataAdapter oleda = new OleDbDataAdapter();
-                DataSet ds = new DataSet();
-                DataTable dt = objConn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
-                string sheetName = string.Empty;
-                if (dt != null)
+                var ds = new DataSet();
+                var dtSheet = objConn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                var sheetName = string.Empty;
+                if (dtSheet != null)
                 {
-                    var tempDataTable = (from dataRow in dt.AsEnumerable()
-                        where !dataRow["TABLE_NAME"].ToString().Contains("FilterDatabase")
-                        select dataRow).CopyToDataTable();
-                    dt = tempDataTable;
-                    totalSheet = dt.Rows.Count;
-                    sheetName = dt.Rows[0]["TABLE_NAME"].ToString();
+                    foreach (DataRow dr in dtSheet.Rows)
+                    {
+                        sheetName = dr["TABLE_NAME"].ToString();
+
+                        if (!sheetName.EndsWith("$"))
+                            continue;
+
+                        var dt = new DataTable {TableName = sheetName};
+                        var da = new OleDbDataAdapter($"SELECT * FROM [{sheetName}]", objConn);
+                        da.Fill(dt);
+                        ds.Tables.Add(dt);
+                    }
                 }
-                cmd.Connection = objConn;
-                cmd.CommandType = CommandType.Text;
-                cmd.CommandText = $"SELECT * FROM [{sheetName}]";
-                oleda = new OleDbDataAdapter(cmd);
-                Console.WriteLine(cmd.CommandText);
-                oleda.Fill(ds, "excelData");
-                dtResult = ds.Tables["excelData"];
+                dtResult = ds.Tables[$"{sheetName}"];
                 objConn.Close();
-                return dtResult; //Returning Datatable  
+                return dtResult;  
             }
         }
     }
